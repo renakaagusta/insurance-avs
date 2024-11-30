65fb7be873b462dbe2d466222fa89c7e51f72603
renaka <renaka@bullseyes-MacBook-Pro.local>
2024-11-30 12:21:20 +0700

    feat: update deployment data

--- operator/zkfetch.ts
+++ operator/zkfetch.ts
@@ -68,8 +68,8 @@ const main = async () => {
         const curl = "curl -X GET 'https://api.npoint.io/c9aa3e8003aceb1b9b52' --header 'content-type: application/json' --header 'x-api-key: SECRET_KEY'"
         const encryptedCurlSecretKey = "04278d1a2ff5452c89e714699a77f81627a9a5875104c0809cf9e0f6dacf5a2c0dd840586a64cfa89b2d104107b5cfd8569f965e6f75cc4eac59955f28d7f1cc70570e667caf233e4eacbc8ebf952fa581e53dae7153515992c681d9df58acfb42";
         const regexExtraction = '\\{"data":\\{"rekts":\\[(?<extractedValue>.*?)\\]\\}\\}';
-        const encryptedApplicationID = "04fab08cf72b5eac277e68f04b45e288d2ee68c4f1696e442e8c03ec7274c7d9da83c0dc614b840e518a4d4fd255a7ee8c9cdb04ceba84eea621ac41265c5a5ac80eacdfe3dd294d813774c1a1ccaedf95e967cbaf554b6c577555e4f64fbd265d4dbf5365c722956afe93d7ad66ca2a87090ddc38e504d6388e6a919b347d2655588e6a11231d2406ddce";
-        const encryptedApplicationSecret = "04927edf4654b8f312250ff8fd6c03cc4fbfdcbfc2d5a27985942bf3921131fac7073d4e9ad43a5c679c3f172e487405aa149b04a6ed907bbbcd33ed2d60cb46720e027bdf21c27ec15469e5f63b1228a75fa641a55b5bcb3feb37dfa050fa987a14c27dca48b92eea19181c325e76d9d3f751018e8743c45fa66fea07f5f0151aab335a14fe29a7ecc2a28c01910fbe5f541ce1f3a1eb4625fbcd824a35503a4d002b";
+        const encryptedApplicationID = "0496e329b071a7894b005740d8e4be15c42167f108ac0c0dbf94cd5ee893891c0e28df2f8be604ba129aaf6c758227a80af2a618769291fdd49ab895d0a3c5de546cdcc9b5e6743866d69bb30ca48ea6af2bf1e4a588e62d7a197953e3c1a4cc5115192085783ec4e7381b4fa250e0b64f80649e63375cac5b4d8675126d655e739d47e4ff989d877a9c4b";
+        const encryptedApplicationSecret = "04283eb9915133830085f9dc05d364218551702a44c5264aac6d389297666268e7afcefd7b4570df7c6e9d249542c255a44b5399a4372c3542119aa728076a8cd0ee8c3a34c614f2ea1f6cb591ca36ea6b7712e74cc25079fe132cc65723e791aa";
         const regexValidation = '"projectName":\\s*"Indodax"';
 
         // Decryption results
